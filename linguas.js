ce6b9b5166fbb21a46a0c0f0d1ec0ff36062463d
ALexandre Junior <alexandregojunior@gmail.com>
2025-07-28 01:23:11 -0300

    languages button creation

---
+++ linguas.js
@@ -0,0 +1,264 @@
+const translations = {
+  pt: {
+    experienciatitulo: "Experiência",
+    experienciaformacao: "Formação",
+    experienciacontato: "Contato",
+    titulo: "Desenvolvedor <br> Backend & <br> Frontend",
+    localizacao: "Localizado em São Paulo 🏨",
+    experiencia: "Experiência",
+    experienciatexto:
+      "Olá, sou Alexandre Garcia, estudante de Análise e Desenvolvimento de Sistemas pela instituição SENAC. <br> Na minha história, sou formado em Arquitetura e Urbanismo, com 12 anos de experiência na área de construção civil. Busco unir minha senioridade na área, minha capacidade de gestão de projetos e organização para impulsionar a busca por esta nova carreira na área de tecnologia e desenvolvimento de sistemas e softwares, com preferência por Backend, e conhecimento em Frontend. <br><br> Tecnologias: <strong>Java</strong> | <strong>Spring Boot</strong> | <strong>MySQL</strong> | <strong>VsCode</strong> | <strong>IntelliJ</strong> |<strong> HTML </strong> | <strong> CSS </strong> ",
+
+    anoempresa1: "2024<br>até<br>o momento",
+    tituloempresa1: "Desenvolvedor Backend e Frontend",
+    textoempresa1:
+      "Desenvolvimento de pequenos projetos, páginas Web, programas e APIs como programador Backend e desenvolvedor Frontend.",
+
+    anoempresa2: "2017<br>até<br>o momento",
+    nomeempresa2: "Nautilo Arquitetura e Gerenciamento",
+    tituloempresa2: "Arquiteto",
+    textoempresa2:
+      "Realização de diversos projetos de arquitetura de diferentes tipologias e tamanhos, realização de todas as etapas de projetos (estudo preliminar e projeto executivo), realização de estudos de materiais e gerenciamento de obras diversas, organização e gestão de equipes de obras, e execução de planilhas e cronogramas.",
+
+    anoempresa3: "2020<br>até<br>o momento",
+    nomeempresa3: "AG Arquitetura",
+    tituloempresa3: "Arquiteto",
+    textoempresa3:
+      "Execução de diversos projetos residenciais e comerciais como arquiteto autônomo em Alexandre Garcia Arquitetura.Dentre eles: projeto de casas unifamiliares, projeto e reforma de apartamentos e estúdios, lojas comerciais, restaurantes, e obras de diversos tipos.",
+
+    formacao: "Formação",
+    formacaotexto:
+      "Estudante de <strong>Análise e Desenvolvimento de Sistemas</strong> pela instituição <strong>SENAC</strong>, e estudante de <strong>Backend</strong> e <strong>Frontend</strong>💻. Formado em <strong>Arquitetura e Urbanismo </strong>, pela Universidade Nove de Julho com 12 anos de experiência na área de construção civil🔨.",
+
+    faculdadetipo1: "Tecnólogo",
+    faculdadecurso1: "Análise e Desenvolvimento de Sistemas",
+
+    faculdadetipo2: "Bacharel",
+    faculdadecurso2: "Arquitetura e Urbanismo",
+
+    faculdadetipo3: "Cursos",
+    faculdadecurso3: "Frontend e Backend",
+
+    fundamentos: "<span>Cisco </span> Fundamentos de Phyton <span>30h</span>",
+
+    idiomas: "Idiomas",
+    lingua1: "Portugues<span> Nativo</span>",
+    lingua2: "Ingles<span> Avançado</span>",
+    lingua3: "Espanhol<span> Intermediário</span>",
+
+    disponivel:
+      "Estou disponível para novos projetos, entre em contato e será um prazer atende-lo(a)👋📲",
+    whatsfale: "Fale conosco",
+  },
+  en: {
+    experienciatitulo: "Experience",
+    experienciaformacao: "Education",
+    experienciacontato: "Contact",
+    titulo: "Backend & <br> Frontend Developer",
+    localizacao: "Located in São Paulo 🏨",
+    experiencia: "Experience",
+    experienciatexto:
+      "Hello, I'm Alexandre Garcia, a student of Systems Analysis and Development at SENAC. In my history, I have a degree in Architecture and Urbanism, with 12 years of experience in the area of civil construction. I seek to merge my seniority in the area, my project management and organization skills to boost the search for this new career in the area of technology and systems and software development, with a preference for Backend, and knowledge in Frontend. <br><br> Technologies: <strong>Java</strong> | <strong>Spring Boot</strong> | <strong>MySQL</strong> | <strong>VsCode</strong> | <strong>IntelliJ</strong> |<strong> HTML </strong> | <strong> CSS </strong>",
+
+    anoempresa1: "2024<br>to<br>date",
+    tituloempresa1: "Backend e Frontend Developer",
+    textoempresa1:
+      "Development of small projects, web pages, programs and APIs as a Backend programmer and Frontend developer.",
+
+    anoempresa2: "2017<br>to<br>date",
+    nomeempresa2: "Nautilo Architecture and Management",
+    tituloempresa2: "Architect",
+    textoempresa2:
+      "Carrying out various architectural projects of different types and sizes, carrying out all project stages (preliminary study and executive project), carrying out material studies and managing various works, organizing and managing construction teams, and executing spreadsheets and schedules.",
+
+    anoempresa3: "2020<br>tobr>date",
+    nomeempresa3: "AG Architecture",
+    tituloempresa3: "Architect",
+    textoempresa3:
+      "Execution of several residential and commercial projects as a freelance architect at Alexandre Garcia Arquitetura. Among them: design of single-family homes, design and renovation of apartments and studios, commercial stores, restaurants, and works of various types.",
+
+    formacao: "Education",
+    formacaotexto:
+      "Student of <strong>Systems Analysis and Development</strong> at <strong>SENAC</strong>, and  <strong>Backend</strong> and <strong>Frontend</strong> student💻. Graduated in <strong>Architecture and Urban Planning </strong>, from Universidade Nove de Julho with 12 years of experience in the construction industry🔨.",
+
+    faculdadetipo1: "Technician",
+    faculdadecurso1: "Systems Analysis and Development",
+
+    faculdadetipo2: "Bachelor",
+    faculdadecurso2: "Architecture and Urban Planning",
+
+    faculdadetipo3: "Courses",
+    faculdadecurso3: "Frontend and Backend",
+
+    intensivos: "Intensive Courses",
+    fundamentos: "<span>Cisco </span> Phyton Fundamentals <span>30h</span>",
+
+    idiomas: "Languages",
+    lingua1: "Portuguese<span> Native</span>",
+    lingua2: "English<span> Advanced</span>",
+    lingua3: "Spanish<span> Intermediate</span>",
+
+    disponivel:
+      "I am available for new projects, get in touch and it will be a pleasure to assist you👋📲",
+    whatsfale: "Call us",
+  },
+
+  es: {
+    experienciatitulo: "Experiencia",
+    experienciaformacao: "Capacitación",
+    experienciacontato: "Contacto",
+    titulo: "Desarrollador <br> Backend y <br> Frontend",
+    localizacao: "Ubicado en São Paulo 🏨",
+    experiencia: "Experiencia",
+    experienciatexto:
+      "Hola, soy Alexandre García, estudiante de Análisis y Desarrollo de Sistemas en SENAC. <br> Soy licenciado en Arquitectura y Urbanismo, con 12 años de experiencia en el sector de la construcción. Busco combinar mi experiencia en el área, mi gestión de proyectos y habilidades organizativas para impulsar la búsqueda de esta nueva carrera en el área de tecnología y desarrollo de sistemas y software, con preferencia por el backend y conocimientos en frontend. <br><br> Tecnologías: <strong>Java</strong> | <strong>Spring Boot</strong> | <strong>MySQL</strong> | <strong>VsCode</strong> | <strong>IntelliJ</strong> | <strong>HTML</strong> | <strong>CSS</strong>",
+
+    anoempresa1: "2024<br>hasta<br>la fecha",
+    tituloempresa1: "Desarrollador backend y frontend",
+    textoempresa1:
+      "Desarrollo de pequeños proyectos, páginas web, programas y APIs como programador Backend y desarrollador Frontend.",
+
+    anoempresa2: "2017<br>hasta<br>la fecha",
+    nomeempresa2: "Nautilo Arquitectura y Gestión",
+    tituloempresa2: "Arquitecto",
+    textoempresa2:
+      "Realizar diversos proyectos arquitectónicos de diferente tipología y tamaño, realizando todas las etapas del proyecto (estudio preliminar y proyecto ejecutivo), realizando estudios de materiales y dirigiendo obras diversas, organizando y gestionando equipos de construcción, y ejecutando hojas de cálculo y cronogramas.",
+
+    anoempresa3: "2020<br>hasta<br>la fecha",
+    nomeempresa3: "AG Arquitectura",
+    tituloempresa3: "Arquitecto",
+    textoempresa3:
+      "Ejecución de diversos proyectos residenciales y comerciales como arquitecto freelance en Alexandre Garcia Arquitetura. Entre ellos: diseño de viviendas unifamiliares, diseño y reforma de apartamentos y estudios, locales comerciales, restaurantes y obras de diversa índole.",
+
+    formacao: "Capacitación",
+    formacaotexto:
+      "Estudiante de <strong>Análisis y Desarrollo de Sistemas</strong> en <strong>SENAC</strong>, con especialización en <strong>Backend</strong> y <strong>Frontend</strong>💻. Graduado en <strong>Arquitectura y Urbanismo </strong>, por la Universidad Nove de Julho, con 12 años de experiencia en el sector de la construcción🔨.",
+
+    faculdadetipo1: "Tecnólogo",
+    faculdadecurso1: "Análisis y Desarrollo de Sistemas",
+
+    faculdadetipo2: "Bachiller",
+    faculdadecurso2: "Arquitectura y Urbanismo",
+
+    faculdadetipo3: "Cursos",
+    faculdadecurso3: "Frontend y Backend",
+
+    fundamentos: "<span>Cisco </span> Fundamentos de Phyton <span>30h</span>",
+
+    idiomas: "Idiomas",
+    lingua1: "Portugués<span> Nativo</span>",
+    lingua2: "Inglés<span> Avanzado</span>",
+    lingua3: "Español<span> Intermedio</span>",
+
+    disponivel:
+      "Estoy disponible para nuevos proyectos, contáctame y será un placer atenderte👋📲",
+    whatsfale: "Contáctenos",
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selector = document.querySelector(".language-selector");
+  const button = document.querySelector("#selected-lang");
+
+  button.addEventListener("click", () => {
+    selector.classList.toggle("open");
+  });
+
+  document.querySelectorAll("#language-options li").forEach((item) => {
+    item.addEventListener("click", () => {
+      const lang = item.getAttribute("data-lang");
+      changeLanguage(lang);
+      selector.classList.remove("open");
+      button.innerHTML = item.innerHTML;
+    });
+  });
+
+  function changeLanguage(lang) {
+    document
+      .querySelectorAll('[data-key="experienciatitulo"]')
+      .forEach((el) => (el.textContent = translations[lang].experienciatitulo));
+    document
+      .querySelectorAll('[data-key="experienciaformacao"]')
+      .forEach(
+        (el) => (el.textContent = translations[lang].experienciaformacao)
+      );
+    document
+      .querySelectorAll('[data-key="experienciacontato"]')
+      .forEach(
+        (el) => (el.textContent = translations[lang].experienciacontato)
+      );
+
+    document.querySelector('[data-key="titulo"]').innerHTML =
+      translations[lang].titulo;
+    document.querySelector('[data-key="localizacao"]').textContent =
+      translations[lang].localizacao;
+    document.querySelector('[data-key="experiencia"]').textContent =
+      translations[lang].experiencia;
+    document.querySelector('[data-key="experienciatexto"]').innerHTML =
+      translations[lang].experienciatexto;
+
+    document.querySelector('[data-key="anoempresa1"]').innerHTML =
+      translations[lang].anoempresa1;
+    document.querySelector('[data-key="tituloempresa1"]').innerHTML =
+      translations[lang].tituloempresa1;
+    document.querySelector('[data-key="textoempresa1"]').innerHTML =
+      translations[lang].textoempresa1;
+
+    document.querySelector('[data-key="anoempresa2"]').innerHTML =
+      translations[lang].anoempresa2;
+    document.querySelector('[data-key="nomeempresa2"]').innerHTML =
+      translations[lang].nomeempresa2;
+    document.querySelector('[data-key="tituloempresa2"]').innerHTML =
+      translations[lang].tituloempresa2;
+    document.querySelector('[data-key="textoempresa2"]').innerHTML =
+      translations[lang].textoempresa2;
+
+    document.querySelector('[data-key="anoempresa3"]').innerHTML =
+      translations[lang].anoempresa3;
+    document.querySelector('[data-key="nomeempresa3"]').innerHTML =
+      translations[lang].nomeempresa3;
+    document.querySelector('[data-key="tituloempresa3"]').innerHTML =
+      translations[lang].tituloempresa3;
+    document.querySelector('[data-key="textoempresa3"]').innerHTML =
+      translations[lang].textoempresa3;
+
+    document.querySelector('[data-key="formacao"]').innerHTML =
+      translations[lang].formacao;
+    document.querySelector('[data-key="formacaotexto"]').innerHTML =
+      translations[lang].formacaotexto;
+
+    document.querySelector('[data-key="faculdadetipo1"]').innerHTML =
+      translations[lang].faculdadetipo1;
+    document.querySelector('[data-key="faculdadecurso1"]').innerHTML =
+      translations[lang].faculdadecurso1;
+
+    document.querySelector('[data-key="faculdadetipo2"]').innerHTML =
+      translations[lang].faculdadetipo2;
+    document.querySelector('[data-key="faculdadecurso2"]').innerHTML =
+      translations[lang].faculdadecurso2;
+
+    document.querySelector('[data-key="faculdadetipo3"]').innerHTML =
+      translations[lang].faculdadetipo3;
+    document.querySelector('[data-key="faculdadecurso3"]').innerHTML =
+      translations[lang].faculdadecurso3;
+
+    document.querySelector('[data-key="intensivos"]').innerHTML =
+      translations[lang].intensivos;
+    document.querySelector('[data-key="fundamentos"]').innerHTML =
+      translations[lang].fundamentos;
+
+    document.querySelector('[data-key="idiomas"]').innerHTML =
+      translations[lang].idiomas;
+    document.querySelector('[data-key="lingua1"]').innerHTML =
+      translations[lang].lingua1;
+    document.querySelector('[data-key="lingua2"]').innerHTML =
+      translations[lang].lingua2;
+    document.querySelector('[data-key="lingua3"]').innerHTML =
+      translations[lang].lingua3;
+
+    document.querySelector('[data-key="disponivel"]').innerHTML =
+      translations[lang].disponivel;
+    document.querySelector('[data-key="whatsfale"]').innerHTML =
+      translations[lang].whatsfale;
+  }
+});
