cf72711b69fbf685d3bb8a2cc24a32319cd32ed0
ALexandre Junior <alexandregojunior@gmail.com>
2025-08-03 04:14:16 -0300

    darkmode corrections

--- darkmode.js
+++ darkmode.js
@@ -1,19 +1,31 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
-}
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
 
-if(darkmode === "active") enableDarkmode()
+if (darkmode === "active") enableDarkmode();
 
 themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+  darkmode = localStorage.getItem("darkmode");
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+
+  const scrollY = window.scrollY || window.pageYOffset;
+  const viewportHeight = window.innerHeight;
+
+  // Se rolou mais que a altura da tela + 800px
+  if (scrollY > viewportHeight + 700) {
+    scrollToTop(1000);
+  }
+});
